4a1cd6a3dc958b826cc0af549f313e66fa5c02f0
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-09-06 11:49:55 +0200

    First basic usage example

--- examples/kyma-on-btp-basic/variables.tf
+++ examples/kyma-on-btp-basic/variables.tf
@@ -1,36 +1,46 @@
 # we're using uppercase variable names, since in some cases (e.g Azure DevOps) the system variables are forced to be uppercase
 # TF allows providing variable values as env variables of name name, case sensitive
 
+variable "BTP_GLOBAL_ACCOUNT" {
+  type        = string
+  description = "Global account name"
+  default     = "global-account-guid"
+}
+
+variable "BTP_BOT_USER" {
+  type        = string
+  description = "Bot account name"
+  default     = "email@domain.com"
+}
+
+variable "BTP_BOT_PASSWORD" {
+  type        = string
+  description = "Bot account password"
+  default     = "password"
+}
+
+variable "BTP_BACKEND_URL" {
+  type        = string
+  description = "BTP backend URL"
+  default     = "https://cpcli.cf.sap.hana.ondemand.com"
+}
+
+variable "BTP_SUBACCOUNT" {
+  type        = string
+  description = "Subaccount name"
+  default     = "subaccount-name"
+}
+
 variable "BTP_KYMA_PLAN" {
   type        = string
   description = "Plan name"
   default     = "gcp"
 }
 
-#Deprecated
-variable "BTP_SUBACCOUNT" {
-  type        = string
-  description = "Subaccount name"
-  default     = null
-}
-
-variable "BTP_USE_SUBACCOUNT_ID" {
-  type        = string
-  description = "Subaccount name"
-  default     = null
-}
-
-
-variable "BTP_NEW_SUBACCOUNT_NAME" {
-  type        = string
-  description = "Subaccount name"
-  default     = null
-}
-
-variable "BTP_NEW_SUBACCOUNT_REGION" {
+variable "BTP_SA_REGION" {
   type        = string
   description = "Region name"
-  default     = null
+  default     = "us31"
 }
 
 variable "BTP_CUSTOM_IAS_TENANT" {
@@ -51,27 +61,8 @@ variable "BTP_KYMA_REGION" {
   default     = "us-central1"
 }
 
-variable "BTP_BOT_USER" {
-  type        = string
-  description = "Bot account name"
-  default     = "email@domain.com"
-}
-
-variable "BTP_BOT_PASSWORD" {
-  type        = string
-  description = "Bot account password"
-  default     = "password"
-}
-
 variable "BTP_PROVIDER_SUBACCOUNT_ID" {
   type        = string
   description = "Subaccount ID"
   default = null
-}
-
-#deprecated
-variable "BTP_SA_REGION" {
-  type        = string
-  description = "Region name"
-  default     = "us31"
 }
