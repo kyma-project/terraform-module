9db1dd51cf7324420c1e403da5db5459c93a3524
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-11-22 14:51:21 +0100

    fix provider version constraints

--- examples/kyma-on-btp-with-custom-oidc/main.tf
+++ examples/kyma-on-btp-with-custom-oidc/main.tf
@@ -2,18 +2,15 @@ terraform {
   required_providers {
     btp = {
       source  = "SAP/btp"
-      version = "1.8.0"
     }
     jq = {
       source  = "massdriver-cloud/jq"
     }
     http = {
       source = "hashicorp/http"
-      version = "3.4.5"
     }
     http-full = {
       source = "salrashid123/http-full"
-      version = "1.3.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
--- provider.tf
+++ provider.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     btp = {
       source  = "SAP/btp"
+      version = ">= 1.6.0"
     }
     jq = {
       source  = "massdriver-cloud/jq"
     }
     http = {
       source = "hashicorp/http"
-      version = "3.4.5"
+      version = ">= 3.4.5"
     }
     http-full = {
       source = "salrashid123/http-full"
