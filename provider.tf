dc28d33dc646a0fd62b8a2135379cf6c7af2c985
Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
2024-11-26 10:40:14 +0100

    cleanup provider (#53)

--- provider.tf
+++ provider.tf
@@ -4,9 +4,6 @@ terraform {
       source  = "SAP/btp"
       version = ">= 1.6.0"
     }
-    jq = {
-      source  = "massdriver-cloud/jq"
-    }
     http = {
       source = "hashicorp/http"
       version = ">= 3.4.5"
