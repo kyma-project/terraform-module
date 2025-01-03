2edb264e0f701fc93af4fb35eaeb7b8b3c05e493
Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>
2025-01-03 18:29:19 +0100

    Bump tf module in gh action (#75)

--- .github/actions/create-sap-btp-kyma/main.tf
+++ .github/actions/create-sap-btp-kyma/main.tf
@@ -28,7 +28,7 @@ provider "btp" {
 }
 
 module "kyma" {
-  source = "git::https://github.com/kyma-project/terraform-module.git?ref=v0.4.0"
+  source = "git::https://github.com/kyma-project/terraform-module.git?ref=v0.4.1"
   BTP_KYMA_PLAN = var.BTP_KYMA_PLAN
   BTP_NEW_SUBACCOUNT_NAME = var.BTP_NEW_SUBACCOUNT_NAME
   BTP_KYMA_REGION = var.BTP_KYMA_REGION
